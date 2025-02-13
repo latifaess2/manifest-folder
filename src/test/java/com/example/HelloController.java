06f53b84940adff872364cbbd07d03a18564c816
latifa <latifaessabbar02@gmail.com>
2025-02-13 19:13:35 +0100

    change test

--- src/test/java/com/example/HelloController.java
+++ src/test/java/com/example/HelloController.java
@@ -1,15 +1,25 @@
 package com.example.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import static org.assertj.core.api.Assertions.assertThat;
 
-@RestController
-@RequestMapping("/api")
-public class HelloController {
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello, World!";
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HelloControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void testSayHello() {
+        String response = this.restTemplate.getForObject("http://localhost:" + port + "/api/hello", String.class);
+        assertThat(response).isEqualTo("Hello, World!");
     }
 }
